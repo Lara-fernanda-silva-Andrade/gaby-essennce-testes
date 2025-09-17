27ad231a88c78dcd308af799efa096711b0de069
Fimozzie2 <mich.dutra99@gmail.com>
2025-09-16 23:24:03 -0300

    alteracao do css

--- backend/js/db.js
+++ backend/js/db.js
@@ -1,16 +1,5 @@
 const mysql = require('mysql2/promise');
 
-// async function conectar() {
-//     const conexao = await mysql.createConnection({
-//         host: 'localhost',
-//         port: '3306',
-//         user: 'root',
-//         password: '',
-//         database: 'gaby',
-//     });
-
-//     return conexao;
-// }
 
 async function conectar() {
     const conexao = await mysql.createConnection({
